a87d0606618a538e94ab5e522cc60cdaeae8aa52
Nicolas Kenny <nicolasdkenny@gmail.com>
2021-04-24 10:47:21 -0300

    2d commit

---
+++ routes/Post.routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const server = Router();
+
+server.post('/')
+
+export default server;
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from 'express';
 
 const server = express();
 
+server.use(express.json());
+
 server.listen(3000, () => {
 	console.log(`Sevidor en http://localhost://3000`);
 });
-
